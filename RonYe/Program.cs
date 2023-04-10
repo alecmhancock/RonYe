34f3cde0cb7fdbf0b4711740039c0e921a8e5302
Alec Hancock <alec.m.hancock@gmail.com>
2023-04-10 11:56:55 -0500

    Assignment complete and gamified.

--- RonYe/Program.cs
+++ RonYe/Program.cs
@@ -1,3 +1,65 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using Newtonsoft.Json.Linq;
+using static System.Net.WebRequestMethods;
 
+
+var ronScore = 0;
+var yeScore = 0;
+var client = new HttpClient();
+var quote = new RonYe.QuoteGen(client);
+
+Console.WriteLine("Let's play RonYe!");
+
+Console.WriteLine();
+
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine($"Kanye: {quote.Kanye()}");
+
+    Console.WriteLine($"Ron Swanson: {quote.RonSwanson()}");
+
+    Console.WriteLine();
+
+    Console.WriteLine($"Round {i+1}: Who won this round, Kanye or Ron Swanson?");
+    var userInput = Console.ReadLine().ToLower();
+    bool again = true;
+    while (again)
+    {
+        again = false;
+
+        switch (userInput.ToLower())
+        {
+            case "ron swanson": ronScore++;
+                break;
+            case "ron": ronScore++;
+                break;
+            case "kanye": yeScore++;
+                break;
+            case "ye": yeScore++;
+                break;
+            default:
+                again = true;
+                break;
+        }
+    }
+    
+}
+Console.WriteLine();
+Console.WriteLine($"Ron's Score: {ronScore}");
+Console.WriteLine();
+Console.WriteLine($"Kayne's Score: {yeScore}");
+
+if (ronScore > yeScore)
+{
+    Console.WriteLine("Ron Swanson wins again.");
+}
+
+else if (yeScore > ronScore)
+{
+    Console.WriteLine("Ye reigns supreme.");
+}
+
+else
+{
+    Console.WriteLine("Well this is a logic bug. Glad you were able to find this.");
+}
---
+++ RonYe/QuoteGen.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Newtonsoft.Json.Linq;
+
+namespace RonYe
+{
+	public class QuoteGen
+	{
+		private HttpClient _client;
+
+		public QuoteGen(HttpClient client)
+		{
+			_client = client;
+		}
+
+		public string Kanye()
+		{
+            var kanyeURL = "https://api.kanye.rest/";
+            var kanyeResponse = _client.GetStringAsync(kanyeURL).Result;
+            var kanyeQuote = JObject.Parse(kanyeResponse).GetValue("quote").ToString();
+			return kanyeQuote;
+        }
+
+		public string RonSwanson()
+		{
+            var swansonURL = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
+            var swansonResponse = _client.GetStringAsync(swansonURL).Result;
+            var ronQuote = JArray.Parse(swansonResponse).ToString().Replace('[', ' ').Replace(']', ' ').Trim();
+			return ronQuote;
+        }
+	}
+}
+
